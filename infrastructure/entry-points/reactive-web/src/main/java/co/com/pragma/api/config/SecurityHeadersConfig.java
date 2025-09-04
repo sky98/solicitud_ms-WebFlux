224d7eaf9cffb9b98fd673bf324ba2960af48c05
sehuanez <>
2025-09-03 21:25:58 -0500

    - Se "limpian" importaciones en desuso del SecurityHEaderConfig. - Se ajusta PATH del servicio expuesto.

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/config/SecurityHeadersConfig.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/config/SecurityHeadersConfig.java
@@ -7,15 +7,11 @@ import co.com.pragma.api.security.RequestTokenFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilterChain;
-import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
